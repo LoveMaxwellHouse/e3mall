ccb7c39038ea687e098994c2e327b248165ab332
lover <@Lenovo-PC>
2018-02-03 20:18:15 +0800

    e3-manager

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
@@ -0,0 +1,24 @@
+package cn.e3.manager.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3.manager.service.ItemService;
+import cn.e3.mapper.TbItemMapper;
+import cn.e3.pojo.TbItem;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	//注入商品mapper接口代理对象
+	@Autowired
+	private TbItemMapper tbItemMapper;
+	
+	public TbItem findItemById(Long itemId) {
+		//根据id查询商品数据
+		TbItem item = tbItemMapper.selectByPrimaryKey(itemId);
+		
+		return item;
+	}
+
+}
